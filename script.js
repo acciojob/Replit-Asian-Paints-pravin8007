36bcf241f52465cdb54440f161735f63ed2921b5
Pravin Patil <106026547+pravin8007@users.noreply.github.com>
2023-10-08 10:45:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+  const changeButton = document.getElementById('change_button');
+        const resetButton = document.getElementById('reset_button');
+
+        changeButton.addEventListener('click', () => {
+            const blockId = document.getElementById('block_id').value;
+            const colorId = document.getElementById('colour_id').value;
+            const gridItem = document.getElementById(blockId);
+
+            if (gridItem) {
+                document.querySelectorAll('.grid-item').forEach(item => {
+                    item.style.backgroundColor = 'transparent';
+                });
+
+                gridItem.style.backgroundColor = colorId;
+            }
+        });
+
+        resetButton.addEventListener('click', () => {
+            document.querySelectorAll('.grid-item').forEach(item => {
+                item.style.backgroundColor = 'transparent';
+            });
+        });
