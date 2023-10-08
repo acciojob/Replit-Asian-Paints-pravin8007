c2715322e0791c9d0a883e4a38424ad36bb67486
Pravin Patil <106026547+pravin8007@users.noreply.github.com>
2023-10-08 10:49:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,8 @@
-//your JS code here. If required.
-  const changeButton = document.getElementById('change_button');
-        const resetButton = document.getElementById('reset_button');
+const blockIdInput = document.getElementById('block_id');
+const colorIdInput = document.getElementById('colour_id');
 
-        changeButton.addEventListener('click', () => {
-            const blockId = document.getElementById('block_id').value;
-            const colorId = document.getElementById('colour_id').value;
-            const gridItem = document.getElementById(blockId);
-
-            if (gridItem) {
-                document.querySelectorAll('.grid-item').forEach(item => {
-                    item.style.backgroundColor = 'transparent';
-                });
-
-                gridItem.style.backgroundColor = colorId;
-            }
-        });
-
-        resetButton.addEventListener('click', () => {
-            document.querySelectorAll('.grid-item').forEach(item => {
-                item.style.backgroundColor = 'transparent';
-            });
-        });
+if (blockIdInput && colorIdInput) {
+  // Add your event listeners here
+} else {
+  console.error('One or both inputs are missing.');
+}
